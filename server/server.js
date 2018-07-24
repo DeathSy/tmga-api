f84433ce7b6809501527830d94af6c49d44daa30
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-07-24 14:47:27 +0700

    [EDIT] ssh command sequence

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
+    console.log(process.env.NODE_ENV);
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
